1271f6ecc4778eb3e3bca6a9c887011dd06f54f5
Kunal Kapoor <k.kapoor@sap.com>
2019-03-07 17:24:34 +0530

    Update bootstrap.sh

--- Dockerfiles/all_in_one/scripts/bootstrap.sh
+++ Dockerfiles/all_in_one/scripts/bootstrap.sh
@@ -5,6 +5,7 @@ echo "Starting mysql server"
 usermod -d /var/lib/mysql/ mysql
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+mysqld --initialize-insecure
 service mysql start &
 while true
 do
